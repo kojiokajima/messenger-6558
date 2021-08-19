d7b7dd0d478a0ab15a68fce0ebac581a11da333c
Koji Okajima <koji970124@gmail.com>
2021-08-19 08:58:46 -0700

    fixed message display and order

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import { Box } from "@material-ui/core";
 import { SenderBubble, OtherUserBubble } from "../ActiveChat";
 import moment from "moment";
 
+const useStyles = makeStyles(() => ({
+  root: {
+    display: "flex",
+    flexDirection: "column-reverse",
+  }
+}))
+
 const Messages = (props) => {
+  const classes = useStyles()
   const { messages, otherUser, userId } = props;
 
   return (
-    <Box>
+    <Box className={classes.root}>
       {messages.map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
